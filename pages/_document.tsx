1108cb5129b012b9199fd1ef6c1662a8c15f2df9
hayrulla huseynov <hayrullahuseynovbusiness@gmail.com>
2022-02-28 14:43:07 +0500

    added new metas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="layout" content="home"/>
+<meta name="keywords" content="Hayrulla,Huseynov,blog,personal,developer,hayrullah,hayrulla huseynov"/>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
